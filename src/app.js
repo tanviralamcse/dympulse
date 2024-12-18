8b11819942a9efb354c3b0d69c9297878a5dc890
tanviralamcse <tanviralam.cse@gmail.com>
2024-12-18 13:28:50 +0100

    added a new endpoint

--- src/app.js
+++ src/app.js
@@ -56,8 +56,8 @@ app.get("/", (req, res) => {
 //     }
 //   });
 
-// Use Routes
-//app.use("/api", contactRoutes); // Prefix all routes with /api
+//Use Routes
+app.use("/api", contactRoutes); // Prefix all routes with /api
 
 // Export app to use in server.js
 module.exports = app;
