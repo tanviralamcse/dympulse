b428626af5f0e92604351663fa7a516bca5cb474
tanviralamcse <tanviralam.cse@gmail.com>
2024-12-18 13:26:47 +0100

    added a new endpoint

--- src/app.js
+++ src/app.js
@@ -39,22 +39,22 @@ app.get("/", (req, res) => {
   res.send("API is running! Please use `/api` for endpoints.");
 });
 
-// Route to test database connection
-app.get("/test", async (req, res) => {
-    try {
-      const result = await pool.query('SELECT NOW()'); // Simple query to test connection
-      res.status(200).json({
-        message: "Database connection is successful",
-        time: result.rows[0].now // Show current time from database to verify connection
-      });
-    } catch (error) {
-      console.error("Database connection error:", error);
-      res.status(500).json({
-        message: "Failed to connect to the database",
-        error: error.message
-      });
-    }
-  });
+// // Route to test database connection
+// app.get("/test", async (req, res) => {
+//     try {
+//       const result = await pool.query('SELECT NOW()'); // Simple query to test connection
+//       res.status(200).json({
+//         message: "Database connection is successful",
+//         time: result.rows[0].now // Show current time from database to verify connection
+//       });
+//     } catch (error) {
+//       console.error("Database connection error:", error);
+//       res.status(500).json({
+//         message: "Failed to connect to the database",
+//         error: error.message
+//       });
+//     }
+//   });
 
 // Use Routes
 //app.use("/api", contactRoutes); // Prefix all routes with /api
