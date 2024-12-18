ff38dcc88d845588c1a9d8c07fd61cfe011e7687
tanviralamcse <tanviralam.cse@gmail.com>
2024-12-18 13:24:02 +0100

    added a new endpoint

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config(); // Load environment variables
+const pool = require("../config/db");
+
 
 const contactRoutes = require("./routes/contactRoutes"); // Import routes
 
@@ -33,13 +35,29 @@ app.get("/debug-env", (req, res) => {
     res.json({ databaseUrl: process.env.DATABASE_URL || "Not Set" });
 });
 
-
 app.get("/", (req, res) => {
   res.send("API is running! Please use `/api` for endpoints.");
 });
 
+// Route to test database connection
+app.get("/test", async (req, res) => {
+    try {
+      const result = await pool.query('SELECT NOW()'); // Simple query to test connection
+      res.status(200).json({
+        message: "Database connection is successful",
+        time: result.rows[0].now // Show current time from database to verify connection
+      });
+    } catch (error) {
+      console.error("Database connection error:", error);
+      res.status(500).json({
+        message: "Failed to connect to the database",
+        error: error.message
+      });
+    }
+  });
+
 // Use Routes
-app.use("/api", contactRoutes); // Prefix all routes with /api
+//app.use("/api", contactRoutes); // Prefix all routes with /api
 
 // Export app to use in server.js
 module.exports = app;
